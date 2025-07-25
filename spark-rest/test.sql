149ace112d05fcd57fcf7055297d02d380f45b9b
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2025-07-25 15:04:59 +0000

    Updated demo script for spark

--- spark-rest/test.sql
+++ spark-rest/test.sql
@@ -1,20 +1,3 @@
-CREATE CATALOG iceberg_rest WITH (
-  'type' = 'iceberg',
-  'catalog-type' = 'rest',
-  'uri' = 'http://iceberg-rest:8181',
-  'warehouse' = 's3a://warehouse',
-  's3.endpoint' = 'http://minio:9000',
-  's3.access-key' = 'minioadmin',
-  's3.secret-key' = 'minioadmin',
-  'fs.s3a.impl' = 'org.apache.hadoop.fs.s3a.S3AFileSystem',
-  's3.region' = 'us-east-1',
-  's3.path-style-access' = 'true'
-);
-
-USE CATALOG iceberg_rest;
-
-CREATE DATABASE IF NOT EXISTS demo;
-
 USE demo;
 
 CREATE TABLE vroom.customers (
